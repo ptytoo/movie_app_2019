1076c4db5d3078425dac9a61f02d8666aa01a807
ptytoo <ptytoo@naver.com>
2020-02-28 11:17:02 +0900

    Updates

---
+++ precache-manifest.1192c1a868a61e2434d8737af1cfd6b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db116c2b0fcc72e5e276c0e7ec5ae68",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "7594eb29fde9b369f365",
+    "url": "/movie_app_2019/static/css/main.7a0dde5b.chunk.css"
+  },
+  {
+    "revision": "f451a5ba244c9e61d337",
+    "url": "/movie_app_2019/static/js/2.457c2c71.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_2019/static/js/2.457c2c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7594eb29fde9b369f365",
+    "url": "/movie_app_2019/static/js/main.ab706bba.chunk.js"
+  },
+  {
+    "revision": "bc42afcb986eef44e7a7",
+    "url": "/movie_app_2019/static/js/runtime-main.30c24a73.js"
+  }
+]);
